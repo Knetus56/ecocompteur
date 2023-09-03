a03e701b637aa39a302986f6f37615e7f92c7be2
Knetus56 <98704728+Knetus56@users.noreply.github.com>
2023-09-03 12:02:03 +0200

    update

--- core/class/ecocompteur.class.php
+++ core/class/ecocompteur.class.php
@@ -21,78 +21,76 @@ require_once __DIR__ . '/../../../../core/php/core.inc.php';
 class ecocompteur extends eqLogic
 {
 
-  public static function cron()
-  {
-    foreach (self::byType('ecocompteur') as $compteur) {
-      if ($compteur->getIsEnable() == 1) {
-        $compteur->getInformations();
-      }
-    }
-  }
-
-
-  public function preUpdate()
-  {
-    if ($this->getConfiguration('ipcompteur') == '') {
-      throw new Exception(__('Veuillez entrer une IP', __FILE__));
-    }
-  }
-
-
-  public function postUpdate()
-  {
-    $this->getInformations();
-  }
-
-
-  public function checkCmdOk($_name)
-  {
-    $ecocompteurCmd = ecocompteurCmd::byEqLogicIdAndLogicalId($this->getId(), $_name);
-    if (!is_object($ecocompteurCmd)) {
-      log::add('ecocompteur', 'debug', 'Création de la commande ' . $_name);
-      $ecocompteurCmd = new ecocompteurCmd();
-      $ecocompteurCmd->setName(__($_name, __FILE__));
-      $ecocompteurCmd->setEqLogic_id($this->getId());
-      $ecocompteurCmd->setEqType('ecocompteur');
-      $ecocompteurCmd->setLogicalId($_name);
-      $ecocompteurCmd->setType('info');
-      $ecocompteurCmd->setSubType('numeric');
-      $ecocompteurCmd->setUnite('W');
-      $ecocompteurCmd->setIsVisible('1');
-      $ecocompteurCmd->setIsHistorized(0);
-      $ecocompteurCmd->setTemplate("mobile", 'line');
-      $ecocompteurCmd->setTemplate("dashboard", 'line');
-      $ecocompteurCmd->save();
-    }
-  }
-
-
-  public function getInformations()
-  {
-    $devAddr = 'http://' . $this->getConfiguration('ipcompteur', '') . '/inst.json';
-    $request_http = new com_http($devAddr);
-    $devResult = $request_http->exec(30);
-    log::add('ecocompteur', 'debug', 'getInformations ' . $devAddr);
-    if ($devResult === false) {
-      log::add('ecocompteur', 'info', 'problème de connexion ' . $devAddr);
-    } else {
-      $devResbis = utf8_encode($devResult);
-      $devList = json_decode($devResbis, true);
-      log::add('ecocompteur', 'debug', print_r($devList, true));
-      foreach ($devList as $name => $value) {
-        if ($name == 'data1' || $name == 'data2' || $name == 'data3' || $name == 'data4' || $name == 'data5') {
-          $this->checkCmdOk($name);
-          $this->checkAndUpdateCmd($name, $value);
+    public static function cron()
+    {
+        foreach (self::byType('ecocompteur') as $compteur) {
+            if ($compteur->getIsEnable() == 1) {
+                $compteur->getInformations();
+            }
         }
-      }
     }
-    $this->refreshWidget();
-  }
+
+
+    public function preUpdate()
+    {
+        if ($this->getConfiguration('ipcompteur') == '') {
+            throw new Exception(__('Veuillez entrer une IP', __FILE__));
+        }
+    }
+
+
+    public function postUpdate()
+    {
+        $this->getInformations();
+    }
+
+
+    public function checkCmdOk($_name)
+    {
+        $ecocompteurCmd = ecocompteurCmd::byEqLogicIdAndLogicalId($this->getId(), $_name);
+        if (!is_object($ecocompteurCmd)) {
+            log::add('ecocompteur', 'debug', 'Création de la commande ' . $_name);
+            $ecocompteurCmd = new ecocompteurCmd();
+            $ecocompteurCmd->setName(__($_name, __FILE__));
+            $ecocompteurCmd->setEqLogic_id($this->getId());
+            $ecocompteurCmd->setEqType('ecocompteur');
+            $ecocompteurCmd->setLogicalId($_name);
+            $ecocompteurCmd->setType('info');
+            $ecocompteurCmd->setSubType('numeric');
+            $ecocompteurCmd->setUnite('w');
+            $ecocompteurCmd->setIsVisible('1');
+            $ecocompteurCmd->setIsHistorized(0);
+            $ecocompteurCmd->setTemplate("mobile", 'line');
+            $ecocompteurCmd->setTemplate("dashboard", 'line');
+            $ecocompteurCmd->save();
+        }
+    }
+
+
+    public function getInformations()
+    {
+        $devAddr = 'http://' . $this->getConfiguration('ipcompteur', '') . '/compt.json';
+        $request_http = new com_http($devAddr);
+        $devResult = $request_http->exec(30);
+        log::add('ecocompteur', 'debug', 'getInformations ' . $devAddr);
+        if ($devResult === false) {
+            log::add('ecocompteur', 'info', 'problème de connexion ' . $devAddr);
+        } else {
+            $devResbis = utf8_encode($devResult);
+            $devList = json_decode($devResbis, true);
+            log::add('ecocompteur', 'debug', print_r($devList, true));
+            foreach ($devList as $name => $value) {
+                $this->checkCmdOk($name);
+                $this->checkAndUpdateCmd($name, (int)$value);
+            }
+        }
+        $this->refreshWidget();
+    }
 }
 
 class ecocompteurCmd extends cmd
 {
-  public function execute($_options = array())
-  {
-  }
+    public function execute($_options = array())
+    {
+    }
 }
--- desktop/js/ecocompteur.js
+++ desktop/js/ecocompteur.js
@@ -56,6 +56,12 @@ function addCmdToTable(_cmd) {
   tr += '<input class="tooltips cmdAttr form-control input-sm" data-l1key="unite" placeholder="Unité" title="{{Unité}}" style="width:30%;max-width:80px;display:inline-block;margin-right:2px;">'
   tr += '</div>'
   tr += '</td>'
+    tr += '<td>'
+  if (is_numeric(_cmd.id)) {
+    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> '
+    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> {{Tester}}</a>'
+  }
+
   tr += '</tr>'
   $('#table_cmd tbody').append(tr)
   var tr = $('#table_cmd tbody tr').last()
